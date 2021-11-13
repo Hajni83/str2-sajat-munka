1d655a634f199806dedeb1581e135e7af758e1ad
Hajni83 <maro.hajnalka@gmail.com>
2021-11-13 16:16:36 +0100

    rotate

--- js-3-feladatok-matching-game/script.js
+++ js-3-feladatok-matching-game/script.js
@@ -1,1 +1,29 @@
 'use strict'
+const cards = document.querySelectorAll('.card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.toggle('flip');
+
+    if (!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+    }else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            firstCard.removeEventListener ('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        }else {
+            setTimeout(() =>{
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            },1500);
+        }
+    }
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));

1e3e10b28bea4c189a51fb3f13fadb5dea6641e4
Hajni83 <maro.hajnalka@gmail.com>
2021-11-27 16:08:34 +0100

    datas add

--- js-5-feladatok-zaro-projekt/js/main.js
+++ js-5-feladatok-zaro-projekt/js/main.js
@@ -1,5 +1,7 @@
 'use strict'
 
+let keys = ["id", "first_name", "last_name", "email", "street", "house"];
+
 function getServerData(url) {
      let fetchOptions = {
         method: "GET",
@@ -26,10 +28,19 @@ function fillDataTable(data, userTable){
         console.error(`Table "${userTable}" is not found.`)
         return;
     }
+
+
+    
+    
+
     let tBody = table.querySelector("tbody");
+    tBody.innerHTML = '';
+    let newRow = newUserRow();
+    tBody.appendChild(newRow);
+
     for( let row of data){
         let tr = createAnyElement("tr");
-        for(let k in row){
+        for(let k of keys){
             let td = createAnyElement("td");
             td.innerHTML = row[k];
             tr.appendChild(td);
@@ -80,4 +91,58 @@ function delRow(btn){
             startGetUsers();
         }
     )
+}
+function newUserRow(row){
+    let tr = createAnyElement("tr");
+    for (let k of keys){
+        let td = createAnyElement("td");
+        let input = createAnyElement("input", {
+            class: "form",
+            name: k
+        });
+        td.appendChild(input);
+        tr.appendChild(td);
+    }
+    let newBtn = createAnyElement("button",{
+        class: "success",
+        onclick:"createUser(this)"
+    });
+    newBtn.innerHTML = `<i class="fa fa-plus-circle" aria-hidden="true"></i>`;
+    let td = createAnyElement("td");
+    td.appendChild(newBtn);
+    tr.appendChild(td);
+
+    return tr;
+}
+
+
+function createUser(btn){
+let tr = btn.parentElement.parentElement;
+let data = getRowData(tr);
+delete data.id;
+let fetchOptions = {
+    method:"POST",
+    mode:"cors",
+    cache:"no-cache",
+    headers:{
+        'Content-Type': 'application/json'
+    },
+    body:JSON.stringify(data)
+};
+fetch(`http://localhost:3000/users`, fetchOptions).then(
+    resp => resp.json(),
+    err => console.error(err)
+).then(
+    data => startGetUsers()
+)
+
+}
+
+function getRowData(tr){
+let inputs = tr.querySelectorAll("input");
+let data = {};
+for (let i =0; i< inputs.length; i++){
+    data[inputs[i].name] = inputs[i].value;
+}
+return data;
 }
